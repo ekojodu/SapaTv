8f6895a9b3d74a214194ea7ee141bfbbd17069f8
ekojodu <alphablaise91@gmail.com>
2024-12-12 13:27:05 +0100

    further changes to home.jsx

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -6,12 +6,13 @@ const Home = () => {
 			<div className='background-container'>
 				<div className='home-images'>
 					<img
-						src='./src/assets/images/getwayMb.png'
+						src='../../assets/images/getwayMb.png'
+
 						alt='Image 1'
 						className='home-img'
 					/>
 					<img
-						src='./src/assets/images/get7Days.png'
+						src='../../assets/images/get7Days.png'
 						alt='Image 2'
 						className='home-img'
 					/>
