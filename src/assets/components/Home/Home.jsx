29dbe86a4e69ae448772ea9c6456b6ea0df843e9
ekojodu <alphablaise91@gmail.com>
2024-12-12 13:31:57 +0100

    made changes to the home.jsx

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -1,21 +1,13 @@
 import React from 'react'; // Ensure you create and use this CSS file
-
+import getwayMb from '/src/assets/images/getwayMb.png';
+import get7Days from '/src/assets/images/get7Days.png';
 const Home = () => {
 	return (
 		<div className='home-content'>
 			<div className='background-container'>
 				<div className='home-images'>
-					<img
-						src='../../assets/images/getwayMb.png'
-
-						alt='Image 1'
-						className='home-img'
-					/>
-					<img
-						src='../../assets/images/get7Days.png'
-						alt='Image 2'
-						className='home-img'
-					/>
+					<img src={getwayMb} alt='Image 1' className='home-img' />
+					<img src={get7Days} alt='Image 2' className='home-img' />
 				</div>
 			</div>
 		</div>
